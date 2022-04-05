f294a5750c7b17b194069931c1929d24d3d52307
Matias Carrizo <matias-carrizo@outlook.com>
2022-04-04 22:22:40 -0300

    contador terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting="Welcome to Skullcandy headphone store!"/>
+      <ItemListContainer greeting="Welcome to Skullcandy headphone store!" />
+      <ItemCount stock={25} />
     </>
   );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import "./ItemCount.css";
+const ItemCount = (props) => {
+  const [count, setCount] = useState(1);
+  const onDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
+  const onIncrement = () => {
+    if (count < props.stock) {
+      setCount(count + 1);
+    }
+  };
+  return (
+    <div className="contenedorItem">
+      <h5 className="contenedorItem__titulo">Grind® True Wireless Earbuds</h5>
+      <div className="contenedorItem__contador">
+        <button className="contenedorItem__contador__boton" onClick={onDecrement}>-</button>
+        <span className="contenedorItem__contador__cantidad">{count}</span>
+        <button className="contenedorItem__contador__boton" onClick={onIncrement}>+</button>
+      </div>
+      <button className="contenedorItem__btnAgregarAlCarrito">Agregar al carrito</button>
+    </div>
+  );
+};
+export default ItemCount;
