43e7ff4fcda30e4f668f61a48403a649b0b32b47
Matias Carrizo <matias-carrizo@outlook.com>
2022-04-08 17:22:21 -0300

    estilos agregados a la card del producto

--- src/components/ItemListContainer/ItemList/Item/Item.js
+++ src/components/ItemListContainer/ItemList/Item/Item.js
@@ -1,10 +1,11 @@
+import "./Item.css"
 export const Item = ({title, price, pictureUrl}) => {
   return (
-    <article>
-      <h1>{title}</h1>
-      <img src={pictureUrl} alt={title} />
-      <p>$ {price}</p>
-      <button>Ver detalle del producto</button>
+    <article className="producto">
+      <h1 className="producto__titulo">{title}</h1>
+      <img className="produco__img" src={pictureUrl} alt={title} />
+      <p className="producto__precio">$ {price}</p>
+      <button className="producto__btnDetalle">Ver detalle del producto</button>
     </article>
   )
 }
